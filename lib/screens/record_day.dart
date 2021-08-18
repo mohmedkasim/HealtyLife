27c71ab10312df5ce86f4b7484308a6632f7daf8
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-08-17 16:48:44 -1200

    init files

---
+++ lib/screens/record_day.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DailyRecord extends StatefulWidget {
+  @override
+  _DailyRecordState createState() => _DailyRecordState();
+}
+
+class _DailyRecordState extends State<DailyRecord> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
