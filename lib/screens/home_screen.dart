be5c94e8f78a89dd28233a6f186d3132d734855b
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-08-19 19:05:15 -1200

    add radial to mainScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gym_schedule/ExampleData.dart';
+import 'package:gym_schedule/components/radial_gauge.dart';
 import 'package:gym_schedule/components/user_account_container.dart';
 import 'package:gym_schedule/components/week_progress.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,64 +13,104 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+            child: Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Image.asset(
-                      'assets/images/healthy.png',
-                      width: 40,
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/healthy.png',
+                          width: 40,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "Healthy Life",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: Text(
+                                "This week record",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    UserContainer()
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: 70),
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      primary: false,
+                      shrinkWrap: true,
+                      itemCount: dailyProgress.length,
+                      itemBuilder: (context, index) {
+                        return WeekProgress(
+                            text: dailyProgress[index].text,
+                            value: dailyProgress[index].value);
+                      }),
+                ),
+                RadialGauge(),
+                SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Daily",
+                      style: TextStyle(color: Colors.grey[400]),
                     ),
                     SizedBox(
-                      width: 10,
+                      width: 20,
                     ),
                     Column(
                       children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Healthy Life",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ],
+                        Text(
+                          "Weekly",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(41, 73, 126, 1)),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15.0),
-                          child: Text(
-                            "This week record",
-                            style: TextStyle(color: Colors.grey),
-                          ),
+                        Icon(
+                          Icons.arrow_drop_up_rounded,
+                          color: Color.fromRGBO(41, 73, 126, 1),
                         )
                       ],
                     ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "Monthly",
+                      style: TextStyle(color: Colors.grey[400]),
+                    )
                   ],
-                ),
-                UserContainer()
+                )
               ],
             ),
-            SizedBox(
-              height: 50,
-            ),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxHeight: 100),
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: dailyProgress.length,
-                  itemBuilder: (context, index) {
-                    return WeekProgress(
-                        text: dailyProgress[index].text,
-                        value: dailyProgress[index].value);
-                  }),
-            )
-          ],
+          ),
         ),
       ),
     );
