c40cc1daed797dcaf9b816c1fdd01a6c061a371f
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-08-19 19:00:44 -1200

    remove debug panel

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Healthy life App',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: "josefin",
           primarySwatch: Colors.blue,
